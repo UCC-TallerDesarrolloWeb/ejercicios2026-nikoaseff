5e5ae7d467c43385e8894e4b33a0ff9df5e10db1
nikoaseff <2512183@ucc.edu.ar>
2026-09-14 13:05:54 -0300

    feat: finish the hw

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -5,3 +5,34 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function convertirUnidades(id,valor){
+    if(isNaN(valor)){
+        alert("se ingreso un valor incorrecto: "+id)
+        document.getElementById("pulgada").value = "";
+        document.getElementById("pie").value = "";
+        document.getElementById("yarda").value = "";
+        document.getElementById("metro").value = "";
+        
+    }else if(id === "metro"){
+        document.getElementById("pulgada").value = valor*39.3701
+        document.getElementById("pie").value = valor*3.28084
+        document.getElementById("yarda").value = valor*1.09361
+
+    }else if(id === "pie"){
+        document.getElementById("pulgada").value = valor * 12;
+        document.getElementById("yarda").value = valor / 3;
+        document.getElementById("metro").value = valor * 0.3048;
+
+    }else if(id === "yarda"){
+        document.getElementById("pulgada").value = valor * 36;
+        document.getElementById("pie").value = valor * 3;
+        document.getElementById("metro").value = valor * 0.9144;
+    }
+
+    else if(id === "pulgada"){
+       document.getElementById("pie").value = valor / 12;
+       document.getElementById("yarda").value = valor / 36;
+       document.getElementById("metro").value = valor * 0.0254; 
+    }
+}
